35c7a1553c33a00ff96cc07b080927abecda2383
primetoxinz <primetoxinzz@gmail.com>
2018-01-17 20:27:02 -0500

    Update dashmod. Add more to the test server.

--- src/test.py
+++ src/test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import time
+import time, random
 from networktables import NetworkTables
 
 # To see messages from networktables, you must setup logging
@@ -13,12 +13,15 @@ sd = NetworkTables.getTable("SmartDashboard")
 def valueChanged(table, key, value, isNew):
     print("valueChanged: key: '%s'; value: %s; isNew: %s" % (key, value, isNew))
 sd.addEntryListener(valueChanged)
-
+sd.putString('robotPhase', 'Teleop')
 
 i = 0
 while True:
     i += 1
     sd.putNumber('robotTime', i)
+    sd.putNumber('voltage', random.randint(0,100))
+    sd.putNumber('current', random.uniform(0,100))
+
     
     print('robotTime:', sd.getNumber('robotTime', 'N/A'))
     time.sleep(1)
